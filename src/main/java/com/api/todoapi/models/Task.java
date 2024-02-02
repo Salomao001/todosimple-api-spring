580404481bc1680049897e4f45c9ce5073cec56e
Salomao <eduardo.salomao.584@gmail.com>
2024-02-02 15:46:11 -0300

    Criando Task

---
+++ src/main/java/com/api/todoapi/models/Task.java
@@ -0,0 +1,33 @@
+package com.api.todoapi.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Task {
+
+    public Task(User user, String description) {
+        this.user = user;
+        this.description = description;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NotBlank
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false, updatable = false)
+    private User user;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String description;
+}
