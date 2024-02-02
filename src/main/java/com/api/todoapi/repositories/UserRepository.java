fe19fc57fd945ea73c7abfd9b160d2bebb8ddb97
Salomao <eduardo.salomao.584@gmail.com>
2024-02-02 17:30:37 -0300

    Criando UserRepository

---
+++ src/main/java/com/api/todoapi/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.api.todoapi.repositories;
+
+import com.api.todoapi.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
